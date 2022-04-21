272adfc1ec8baf1eee1a6769eb74176e24ff5588
Nam Phung Thanh <spnether1411@gmail.com>
2022-04-21 09:50:40 +0700

    Fix typo

--- 08_webmaster_hacks/randomquote.sh
+++ 08_webmaster_hacks/randomquote.sh
@@ -2,8 +2,8 @@
 
 # 68 Displaying Random Text
 # randomtext--Given a one-line-per-entry datafile,
-#   randomly picks one line and displays ita. Best used
-#   as aan SSI call within a web page.
+#   randomly picks one line and displays it. Best used
+#   as an SSI call within a web page.
 awkscript="/tmp/randomquote.awk.$$"
 
 if [ $# -ne 1 ] ; then
